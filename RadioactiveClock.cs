f064675095cf9071b1f13bef661cdc89c9b2b9d2
Patryk Holodynski <p.holodynski@gmail.com>
2022-04-26 12:00:50 -1000

    Add files via upload

---
+++ RadioactiveClock.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.Buildings;
+
+namespace MoreClocks.RadioactiveClock
+{
+	public class RadioactiveClockBuilding : Building
+	{
+		private static readonly BluePrint Blueprint = new("Radioactive Clock");
+
+		public RadioactiveClockBuilding()
+			: base(RadioactiveClockBuilding.Blueprint, Vector2.Zero) { }
+	}
+}
